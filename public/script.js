ac5fb366306c81033713295b1449cc4bf37875c5
pakota159 <pakota159@gmail.com>
2017-01-04 20:45:35 +0700

    add section

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 //Define function =========================================
+//Initialize html
 var add = function(element, val){
 		element += '<div class="col-sm-12 col-xs-6 col-md-4">';
 		element += '<input type="text" id="'+val.fieldName+'" value="'+val.fieldName+'" placeholder="Type Name Field">';
@@ -23,7 +24,7 @@ var add = function(element, val){
 
 var createObjective = function(i, val){
 	var element = [];
-	element += '<div class="row objective">';
+	element += '<li class="row obj">';
 	element += '<div class="col-sm-12 col-xs-6 col-md-5">';
 	element += '<input type="text" placeholder="Type Objective" value="'+val.fieldName+'">';
 	element += '</div>';
@@ -33,9 +34,9 @@ var createObjective = function(i, val){
 	element += '</select>';
 	element += '</div>';
 	element += '<div class="col-sm-6 col-xs-1 col-md-1">';
-	element += '<i class="material-icons">clear</i>';
+	element += '<i class="material-icons delete">clear</i>';
 	element += '</div>';
-	element += '</div>';
+	element += '</li>';
 	$('#Objective').closest('li').append(element);
 };
 
@@ -47,13 +48,75 @@ var createField = function(val){
 		element += add(element, val);
 		element += '</div>';
 		element += '</li>';
-		$('.each-field ul').append(element);
+		$('.main-section ul').append(element);
 	}
 };
 
+var createSection = function(val){
+	var allType = ['Textbox', 'Email', 'Select'];
+	var element = [];
+	for(var i = 0; i<val.length; i++){
+		element += '<div class="row">';
+		element += '<div class="col-sm-12 col-xs-6 col-md-4">';
+		element += '<h2>'+val[i]+'</h2>';
+		element += '</div>';
+		element += '</div>';
+		element += '<div class="row">';
+		element += '<div class="col-sm-12 col-md-6">';
+		element += '<input type="text" class="description" placeholder="Type Description">';
+		element += '</div>';
+		element += '</div>';
+		element += '<ul id="section-field-'+val[i].split(' ').join('')+'">';
+		element += '<li></li>';
+		element += '</ul>';
+		$('.sub-section').append(element);
+		element = [];
+		
+		element += '<li>';
+		element += '<div class="row">';
+		element += '<div class="col-sm-12 col-xs-6 col-md-4">';
+		element += '<input type="text" placeholder="Type Name Field">';
+		element += '</div>';
+		element += '<div class="col-sm-12 col-xs-6 col-md-4">';
+		element += '<select name="type">';
+		for(var j = 0; j<allType.length; j++){
+			element += '<option value="'+j+'">'+allType[j]+'</option>';
+		};
+		element += '</select>';
+		element += '</div>';
+		element += '<div class="col-sm-6 col-xs-10 col-md-3">';
+		element += '<select name="require">';
+		element += '<option value="1">Required</option>';
+		element += '<option value="2">Optional</option>';
+		element += '</select>';
+		element += '</div>';
+		element += '<div class="col-sm-6 col-xs-1 col-md-1">';
+		element += '<i class="material-icons delete">clear</i>';
+		element += '</div>';
+		element += '</div>';
+		element += '</li>';
+		$('#section-field-'+val[i].split(' ').join('')).append(element);
+		element = [];
+	}
+	
+};
+
+var createRightPanel = function(val){
+	var element = [];
+	for(var i = 0; i<val.sections.length; i++){
+		console.log(val.sections[i]);
+		element += '<li>'
+		element += '<p>'+val.sections[i]+'</p>'
+		element += '</li>'
+	};
+	$('ol').append(element);
+}
+
+//====================================================================
+//Add element when click button
 var addChoice = function(val){
 	var element = [];
-	element += '<div class="row objective">';
+	element += '<li class="row obj">';
 	element += '<div class="col-sm-12 col-xs-6 col-md-5">';
 	element += '<input type="text" placeholder="Type Objective">';
 	element += '</div>';
@@ -63,9 +126,9 @@ var addChoice = function(val){
 	element += '</select>';
 	element += '</div>';
 	element += '<div class="col-sm-6 col-xs-1 col-md-1">';
-	element += '<i class="material-icons">clear</i>';
+	element += '<i class="material-icons delete">clear</i>';
 	element += '</div>';
-	element += '</div>';
+	element += '</li>';
 	$(val).before(element);
 };
 
@@ -95,19 +158,24 @@ var addField = function(val){
 	element += '</div>';
 	element += '</div>';
 	element += '</li>';
-	$('.each-field').find('#sort-field').append(element);
+	$('.main-section').find('#main-field').append(element);
 };
+
+var addSectionRightPanel = function(val){
+	var element = [];
+	element += '<li>'
+	element += '<p>'+$('.section input').val();+'</p>'
+	element += '</li>'
+	$('ol').append(element);
+}
 
 var deleteElement = function(val){
 	$(val).closest('li').remove();
 };
 
-
-
 //=============================================================
 //Execute function ============================================
 $(document).ready(function() {
-	// $('select').material_select();
 	$.ajax({
 		url: "/data",
 		method: "GET"
@@ -124,8 +192,8 @@ $(document).ready(function() {
 		  }
 		};
 		$(function() {
-		    $( "#sort-field" ).sortable();
-		    $( "#sort-field" ).droppable({
+		    $( "#main-field" ).sortable();
+		    $( "#main-field" ).droppable({
 		    	drop: function(event,ui){
 				    var id = ui.draggable.find('p').attr('id');
 				    for (var key in response) {
@@ -146,27 +214,39 @@ $(document).ready(function() {
 		        }
 		    });
 		    $( "#pre-list" ).sortable({
-		    	connectWith: "#sort-field"
+		    	connectWith: "#main-field"
 		    });
 		    $("#pre-list li").draggable({
-		    	connectToSortable: '#sort-field',
+		    	connectToSortable: '#main-field',
 		    	helper: 'clone',
 		    	scope: "items"
 		    });
 		 });
 		$('#Objective').closest('li').append('<button id="addChoice">Add Choices</button>');
-		$('.each-field').append('<ul><li><div class="row"><div class="col-sm-12"><button id="addField">Add Field</button></div></div></li></ul>')
+		$('.main-section').append('<ul><li><div class="row"><div class="col-sm-12"><button id="addField">Add Field</button></div></div></li></ul>');
+		
+		createSection(response['section'].sections);
+		createRightPanel(response['section']);
 	});
 });
 
-$('.each-field').on('click', '#addChoice', function(){
+$('.main-section').on('click', '#addChoice', function(){
 	addChoice(this);
 });
 
-$('.each-field').on('click', '#addField', function(){
+$('.main-section').on('click', '#addField', function(){
 	addField(this);
 });
 
 $('.main').on('click', 'i.delete', function(){
 	deleteElement(this);
 });
+
+$('.right-panel').on('click', '#add-right-panel', function(){
+	if($('.right-panel input').val() === ''){
+		alert("Please enter a valid section name")
+	}else{
+		addSectionRightPanel(this);
+		createSection([$('.right-panel input').val()])
+	}
+})
